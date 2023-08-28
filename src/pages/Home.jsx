39fdba54a19698382cb94428334419558eb6e3b7
Pieter Deane <deanepieter@gmail.com>
2023-08-28 14:23:52 +0200

    updated styling added paragraph homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,8 @@ import '../styles/Home.scss'
 const Home = () => {
   return (
     <div className="home-container">
-      <h1>welcome to the FD Interest Calculator App</h1>
+      <h1>Welcome to the FD Interest Calculator App</h1>
+      <p>This app will help you calculate your FD interest.</p>
     </div>
   );
 };
